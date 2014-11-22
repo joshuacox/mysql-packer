bfd786e2b3685d7c55d3db6b59ba57b7b371a959
Josh Cox <josh@webhosting.coop>
2014-11-22 17:50:58 -0600

    packer

---
+++ packer/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#rm -r vendor
+#rm Berksfile.lock
+#berks vendor vendor/cookbooks
+#sed -i 's/d-i\ mirror\/http\/proxy\ string/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/' http/debian-7/preseed.cfg
+#/usr/bin/time -v packer build -debug --only=virtualbox-iso postgresql-debian-7.7-i386.json
+#/usr/bin/time -v packer build --only=virtualbox-iso postgresql-debian-7.7-i386.json
+#/usr/bin/time -v packer build -debug --only=qemu postgresql-debian-7.7-i386.json
+#/usr/bin/time -v packer build --only=qemu postgresql-debian-7.7-i386.json
+#sed -i 's/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/d-i\ mirror\/http\/proxy\ string/' http/debian-7/preseed.cfg
+# beeper
+#aplay /usr/share/sounds/alsa/Front_Center.wav
+# ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
+make oltorf
